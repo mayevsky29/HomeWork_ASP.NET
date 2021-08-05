d9e9274e0039d2087db7b29c305be777f938b6f1
mayevskyivan5@gmail.com <mayevskyivan5@gmail.com>
2021-08-05 15:17:00 +0300

    add info with lectures

--- ASP.NET.Homework/Controllers/AnimalController.cs
+++ ASP.NET.Homework/Controllers/AnimalController.cs
@@ -7,27 +7,62 @@ using ASP.NET.Homework.Models;
 using ASP.NET.Homework.Domain;
 using ASP.NET.Homework.Domain.Entities.Catalog;
 using System.Globalization;
+using AutoMapper;
+using Bogus;
+using System.Threading;
 
 namespace ASP.NET.Homework.Controllers
 {
     public class AnimalController : Controller
     {
         private readonly AppEFContext _context;
+        private readonly IMapper _mapper;
 
-        public AnimalController(AppEFContext context)
+        public AnimalController(AppEFContext context,
+            IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
+
+           //GenataAnimal();
         }
+
+        private void GenataAnimal()
+        {
+            int n = 1000;
+            var endDate = DateTime.Now;
+            var startDate = new DateTime(endDate.Year - 2,
+                endDate.Month, endDate.Day);
+
+            var faker = new Faker<Animal>("uk")
+                .RuleFor(x => x.Name, f => f.Person.FullName)
+                .RuleFor(x => x.DateBirth, f => f.Date.Between(startDate, endDate))
+                .RuleFor(x => x.Image, f => f.Image.PicsumUrl())
+                .RuleFor(x => x.Price, f => Decimal.Parse(f.Commerce.Price(100M, 500M)))
+                .RuleFor(x => x.DateCreate, DateTime.Now);
+
+
+            for (int i = 0; i < n; i++)
+            {
+                var animal = faker.Generate();
+                _context.Animals.Add(animal);
+                _context.SaveChanges();
+            }
+        }
+
         public IActionResult Index()
         {
-            List<AnimalsViewModel> model =
-                _context.Animals.Select(x => new AnimalsViewModel
-                {
-                    Id = x.Id,
-                    BirthDay = x.DateBirth,
-                    Image = x.Image,
-                    Name = x.Name
-                }).ToList();
+            var model = _context.Animals
+               .Select(x => _mapper.Map<AnimalsViewModel>(x))
+               .ToList();
+            //List<AnimalsViewModel> model =
+            //    _context.Animals.Select(x => new AnimalsViewModel
+            //    {
+            //        Id = x.Id,
+            //        BirthDay = x.DateBirth,
+            //        Image = x.Image,
+            //        Name = x.Name
+            //    }).ToList();
 
             return View(model);
         }
@@ -54,6 +89,20 @@ namespace ASP.NET.Homework.Controllers
             _context.SaveChanges();
 
             return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Delete(long id)
+        {
+            Thread.Sleep(2000);
+            var item = _context.Animals.SingleOrDefault(x => x.Id == id);
+            if (item != null)
+            {
+                //_context.Remove(item);
+                _context.Animals.Remove(item);
+                _context.SaveChanges();
+            }
+            return Ok();
         }
     }
 }
---
+++ ASP.NET.Homework/Models/AnimalProfile.cs
@@ -0,0 +1,25 @@
+﻿using ASP.NET.Homework.Domain.Entities.Catalog;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET.Homework.Models
+{
+    public class AnimalProfile : Profile 
+    {
+        public AnimalProfile()
+        {
+            CreateMap<Animal, AnimalsViewModel>()
+                .ForMember(x => x.BirthDay,
+                            opt => opt.MapFrom(x => x.DateBirth
+                            .ToString("dd MMMM yyyy", CultureInfo.CreateSpecificCulture("uk"))
+                            ));
+            //.ReverseMap();
+            //CreateMap<Animal, AnimalsViewModel>();
+
+        }
+    }
+}
